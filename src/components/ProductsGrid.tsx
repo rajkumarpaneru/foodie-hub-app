9731f02ba537a99e6f8ec0a8e77f23299133d6a2
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2024-01-09 21:12:01 +0545

    add spacing between product cards

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -21,7 +21,7 @@ const ProductsGrid = ({ productQuery }: Props) => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
-        spacing={4}
+        spacing={6}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
